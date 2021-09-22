ab9c3b094f63cdc8872640386f816d06610aff01
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-09-22 00:37:58 -0400

    Revised method of handling and returning arguments which are files

--- src/cli.js
+++ src/cli.js
@@ -27,29 +27,12 @@ function parseArguments(argsRaw) {
     version: args["--version"] || false,
     help: args["--help"] || false,
     // recursive: args["--recursive"] || false,
-    raw: argsRaw
+    files: []
   };
 
   // Will print out the help dialogue - this flag will not do anything other than print out the help.
   if (values.help) {
-    console.log(
-      "%s",
-      chalk.blue.bold.underline(
-        "Information Directives:"
-      ),
-    );
-
-    console.log(
-      "%s",
-      chalk.cyan(
-        "The Static Site Generator will accept .txt files and convert the data within it to HTML.\nThese HTML files can then be used to host the website\n"
-      ),
-    );
-
-    console.log(
-     "%s \n--version: provides version of package\n--input: flag used to indicate files to convert", chalk.inverse.bold("Flag Directory")
-      );
-    
+    printHelp();
     return;
   }
 
@@ -64,15 +47,52 @@ function parseArguments(argsRaw) {
   }
 
 
+  // filter out all arguments after the --input flag to identify as text files and push to the values.files
+  if (values.input) {
+    //find the location of where the input flag was put, and parse arguments after that as files.
+    let files = argsRaw.splice(argsRaw.indexOf("--i") + 1);
+
+    files.forEach(file => {
+      if (file.includes(".txt")){
+        values.files.push({
+          name: file,
+          type: "txt"
+        })
+      } else if (file.includes(".md")) {
+        values.files.push({
+          name: file,
+          type: "md"
+        })
+      }
+    });
+  }
+
   return values;
   
 }
 
+function printHelp() {
+  console.log(
+    "%s",
+    chalk.blue.bold.underline(
+      "Information Directives:"
+    ),
+  );
 
-function getInputFiles(args) {
-  console.log("received input")
+  console.log(
+    "%s",
+    chalk.cyan(
+      "The Static Site Generator will accept .txt files and convert the data within it to HTML.\nThese HTML files can then be used to host the website\n"
+    ),
+  );
 
+  console.log(
+   "%s \n--version: provides version of package\n--input: flag used to indicate files to convert", chalk.inverse.bold("Flag Directory")
+    );
+  
 }
+
+
 
 async function promptOptions(options) {
   const promptQuestions = [];
@@ -111,19 +131,21 @@ async function promptOptions(options) {
 }
 
 export async function cli(args) {
-  console.log(args)
+  // console.log(args)
   let options = parseArguments(args);
-  console.log(options)
-  // if (options.files[0] === "--i" || options.files === "--input"){
+  // console.log(options)
+  
+  
+  if (options.files[0] === "--i" || options.files === "--input"){
     
-  //   // if (options.recursive) {
-  //   //   await createHtml(options.files.slice(2), options.recursive);
-  //   // } else {
-  //     await createHtml(options.files.slice(1), options.recursive);
-  //   // }
-  // } else {
-  //   options = await promptOptions(options);
-  //   await createHtml(options.files);
-  // }
+    // if (options.recursive) {
+    //   await createHtml(options.files.slice(2), options.recursive);
+    // } else {
+      await createHtml(options.files.slice(1), options.recursive);
+    // }
+  } else {
+    options = await promptOptions(options);
+    await createHtml(options.files);
+  }
   
 }
